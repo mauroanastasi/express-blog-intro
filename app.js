b12e75dc437d0eb4b58276b5fa787e76b92cc9d2
mauroanastasi <mauroanastasi1@gmail.com>
2025-04-16 17:00:41 +0200

    [UPD] creo array con 5 post, importo imgs

--- app.js
+++ app.js
@@ -11,3 +11,32 @@ app.get(`/`, (req,res) => {
 app.listen (port, () => {
     console.log(`Example app listening on port ${port}`)
 })
+
+const post = [
+    {
+        title:"Ciambellone",
+        content:"lorem imsumo clab caja defcon",
+        image:"public/imgs/ciambellone.jpeg",
+        tags:[`#zuccheroavelo`, `#giallo`, `#ciambella`,`#ciambellone`, `#zuccero`]
+    }, {
+        title:"Special crackers",
+        content:"lorem imsumo clab caja defcon dicotomic label natus",
+        image:"public/imgs/cracker_barbabietola.jpeg",
+        tags:[`#fucsia`, `#semi`, `#creckers`, `#barbabietola`, `#zuccero`]
+    }, {
+        title:"Pane fritto della nonna",
+        content:"lorem luna neptum rekiamo lorem imsumo clab caja defcon",
+        image:"public/imgs/pane_fritto_dolce.jpeg",
+        tags:[`#fritto`, `#pane`, `#ricettedellanonna`, `#zuccero`]
+    }, {
+        title:"Special pasta",
+        content:"lorem sole ictur piena cablando lorem luna neptum rekiamo lorem imsumo clab caja defcon",
+        image:"public/imgs/pasta_barbabietola.jpeg",
+        tags:[`#barbabietola`, `#fucsia`, `#pasta`, `#pistacchio`]
+    }, {
+        title:"Torta conviviale",
+        content:"lorem imsumo clab caja defcon  sole ictur piena cablando",
+        image:"public/imgs/torta_paesana.jpeg",
+        tags:[`#ricetteantiche`, `#tradizione`, `#dolcibonta`, `#cioccolato`]
+    }
+]
